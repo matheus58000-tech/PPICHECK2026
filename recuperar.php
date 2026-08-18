31b9c1e6113cd326940a232c7fa4ac6428236919
Tobias <tobias.15024@aluno.iffar.edu.br>
2026-08-17 21:44:57 -0300

    Update recuperar.php

--- recuperar.php
+++ recuperar.php
@@ -5,19 +5,16 @@ require_once 'conexao.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = trim($_POST['email_recuperacao']);
 
-    // Verifica se o e-mail existe no banco
     $stmt = $conn->prepare("SELECT id_user FROM Usuarios WHERE Email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        // E-mail encontrado! 
         $_SESSION['msg_sucesso'] = "Instruções enviadas para o seu e-mail!";
         header("Location: index.php");
         exit();
     } else {
-        // E-mail não encontrado
         $_SESSION['erro_campo'] = "recuperacao";
         $_SESSION['erro_msg'] = "Este e-mail não consta no nosso sistema.";
         header("Location: index.php");
